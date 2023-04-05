72c2946ae406f13a9c429354c36108ddffd1b491
cadenedam <cedam@purdue.edu>
2023-04-05 14:18:16 -0400

    minor setup

--- Marketplace.java
+++ Marketplace.java
@@ -15,9 +15,46 @@ public class Marketplace {
             scan.nextLine();
 
             if (user == 1) {
+                System.out.println("What would you like to do?");
+                System.out.println("1. View marketplace");
+                System.out.println("2. Search for a product");
+                System.out.println("3. Logout");
+                int selection = scan.nextInt();
+                scan.nextLine();
+
+                switch(selection) {
+                    case 1:
+                    break;
+                    case 2:
+                    break;
+                    case 3:
+                    break;
+                    default:
+                    System.out.println("Please enter a valid input");
+                }
 
             } else if (user == 2) {
-    
+                System.out.println("What would you like to do?");
+                System.out.println("1. Create a product");
+                System.out.println("2. Edit/Delete a product");
+                System.out.println("3. Create a store");
+                System.out.println("4. View stores");
+                int selection = scan.nextInt();
+                scan.nextLine();
+
+                switch(selection) {
+                    case 1:
+                    break;
+                    case 2:
+                    break;
+                    case 3:
+                    break;
+                    case 4:
+                    break;
+                    default:
+                    System.out.println("Please enter a valid input");
+                }
+
             } else {
                 System.out.println("That's not a valid input!");
                 validUser = false;
--- Sellers.java
+++ Sellers.java
@@ -1,9 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
 public class Sellers {
 
-
-
+    public String username;
+    public String password;
+    public ArrayList<String> stores = new ArrayList<String>();
 
     public String addStore(String store) {
-        
+        stores.add(store);
     }
 }
