1eeb9d39570df124d9d9e9a8fc15c02b2fcd778e
shall1018 <129920166+shall1018@users.noreply.github.com>
2023-04-09 23:26:14 -0400

    Update SellersDashboard.java

--- SellersDashboard.java
+++ SellersDashboard.java
@@ -1,39 +1,59 @@
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class SellersDashboard {
+    public String [] stores;
+    public String [] storeCustomers;
+    public String [] itemsSold;
+    public String [] products;
+    public String [] sales;
 
-    public String username;
-    public String password;
-
-    public SellersDashboard(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public SellersDashboard(String[] stores, String[] storeCustomers, String[] itemsSold, String [] products, String [] sales) {
+        this.stores = stores;
+        this.storeCustomers = storeCustomers;
+        this.itemsSold = itemsSold;
+        this.products = products;
+        this.sales = sales;
     }
 
-    public void printSellersDashboard() throws IOException {
-        Sellers seller = new Sellers(username, password);
-        String stores = seller.getStores();
-        System.out.println("Dashboard:" + "\n");
-        String [] storesList = stores.split( "[\\s,]+" );
+    //stores - each store index corresponds to the storeCustomers index
+    //storeCustomers - list of customers for the store/index, separated by "," plz (no space)
+    //itemsSold - index still corresponds to each store - list of number of items bought by customer, separated by "," where the number corresponds to "index" of the customer
+    //products - index still corresponds to each store - list of products by that store separated by ","
+    //sales - index still corresponds to each store - list of number of sales for each product separated by "," where the number corresponds to "index" of the product
 
-        for (int i = 0; i < storesList.length; i++) {
-            System.out.println("Store:" + storesList[i] + "\n");
-            //get customers from certain store
-            String customers = storeCustomers[i]; // get from sellers
-            String [] customerList = customers.split( "[\\s,]+" );
+    public void viewDashboard() {
+        System.out.println("Dashboard:" + "\n");
+        for (int i = 0; i < stores.length; i++) {
+            System.out.println("Store:" + stores[i] + "\n");
 
             System.out.println("Customer Data");
-            for (int j = 0; j < customerList.length; j++) {
-                int itemCount = 0; // update later
-                System.out.println("Customer:" + customerList[i] + "Items Purchased: " + itemCount + "\n");
+            List<String> customerList = Arrays.asList(storeCustomers[i].split(","));
+            String[] customers = customerList.toArray(new String[0]);
+            List<String> items = Arrays.asList(itemsSold[i].split(","));
+            String[] itemsCount = items.toArray(new String[0]);
+            List<String> prod = Arrays.asList(products[i].split(","));
+            String[] storeProducts = prod.toArray(new String[0]);
+            List<String> sale = Arrays.asList(sales[i].split(","));
+            String[] soldCount = sale.toArray(new String[0]);
+            boolean printJ = false;
+            for (int j = 0; j < customers.length; j++) {
+                System.out.println("Customer:" + customers[j] + "Items Purchased: " + itemsCount[j]);
+                printJ = true;
             }
-
-            // String [] storeProducts = get from sellers.java
-            System.out.println("Product Data");
-            for (int k = 0; k < storeProducts.length; k++) { // get from sellers
-                int soldCount = 0; // update later
-                System.out.println("Product:" + storeProducts[i] + "Number of Sales: " + soldCount + "\n");
+            if (!printJ) {
+                System.out.println("~No Customer Data~");
             }
+            System.out.println("\nProduct Data");
+            boolean printK = false;
+            for (int k = 0; k < products.length; k++) {
+                System.out.println("Product:" + storeProducts[k] + "Number of Sales: " + soldCount[k]);
+                printK = true;
+            }
+            if (!printK) {
+                System.out.println("~No Product Data~");
+            }
+            System.out.println("\n");
         }
     }
 }
