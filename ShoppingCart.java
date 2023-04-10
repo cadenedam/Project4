3948394cb0fa2890b9447dee7503c7550f3ef7bf
shall1018 <129920166+shall1018@users.noreply.github.com>
2023-04-09 21:19:47 -0400

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,23 +1,57 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCart {
 
     public String username;
     public String password;
-    public String [] shoppingCart;
 
     public ShoppingCart(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    void addToCart(String product) {
-        ArrayList<String> cart = new ArrayList<String>();
-        for (int i = 0; i < shoppingCart.length; i++) {
-            cart.add(shoppingCart[i]);
+    public void createCart() throws IOException {
+        // if you create the shopping cart when the customer account is created,
+        // it will be simple to prevent calling it multiple times
+        File file = new File("shoppingCart." + username + ".txt");
+        file.createNewFile();
+    }
+
+    void addToCart(String store, String product, int quantity, double price) throws IOException {
+        PrintWriter pw = new PrintWriter(new FileWriter("shoppingCart." + username + ".txt", true), true);
+        pw.write(store + ";" + product + ";" + quantity + ";" + price);
+        pw.println();
+        pw.close();
+    }
+
+    void removeFromCart(String store, String product, int quantity, double price) throws IOException {
+        String remove = String.format("%s;%s;%d;%f", store, product, quantity, price);
+        File tempFile = new File("myTempFile.txt");
+        File inFile = new File("shoppingCart." + username + ".txt");
+        BufferedReader br = new BufferedReader(new FileReader(inFile));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+        String line;
+        while((line = br.readLine()) != null) {
+            String trimmedLine = line.trim();
+            if(!trimmedLine.equals(remove)) {
+                bw.write(line + System.getProperty("line.separator"));
+            }
         }
-        cart.add(product);
-        String[] list = cart.toArray(new String[0]);
-        this.shoppingCart = list;
+        bw.close();
+        br.close();
+        inFile.delete();
+        tempFile.renameTo(inFile);
+    }
+
+    public String [] getCart() throws IOException {
+        List<String> cart = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("shoppingCart." + username + ".txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            cart.add(line);
+        }
+        return cart.toArray(new String[0]);
     }
 }
