1e363a56e750e5c5e323747c855f8e4dbe8ffdb1
Rneupane0056 <roshanneupane2003@gmail.com>
2023-04-10 10:08:10 -0400

    Just some questions about functionality for Sellers and Marketplace classes
    Please note that there are still errors within the Marketplace class

---
+++ Selection.java
@@ -0,0 +1,175 @@
+import java.io.*;
+import java.util.*;
+
+public class Selection {
+
+    public String username;
+    public String password;
+    private String filename;
+    private List<String> products;
+    File fileProducts = new File("products.txt");
+    File filePurchased = new File("purchases.txt");
+
+
+//    public Selection(List<String> products) {
+//        this.products = products;
+//    }
+    //the constructor above may not be necessary
+
+    public ArrayList<String> readFile(String fileName) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+            while (line != null) {
+                list.add(line);
+                line = bfr.readLine();
+            }
+            bfr.close();
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    //allows the seller to export products for their store
+
+    public ArrayList<String> writeProductsFile(Scanner in) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedWriter productListWriter = new BufferedWriter(new FileWriter(fileProducts));
+        String newList = "";
+        productListWriter.write(newList);
+        productListWriter.close();
+        try {
+            FileReader fr = new FileReader(fileProducts);
+            BufferedReader bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+            while (line != null) {
+                list.add(line);
+                line = bfr.readLine();
+            }
+            bfr.close();
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    //allows the customer to export a file with their purchase history
+
+    public ArrayList<String> writePurchasedFile(Scanner in) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedWriter purchaseListWriter = new BufferedWriter(new FileWriter(filePurchased));
+        String newList = "";
+        purchaseListWriter.write(newList);
+        purchaseListWriter.close();
+        try {
+            FileReader fr = new FileReader(filePurchased);
+            BufferedReader bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+            while (line != null) {
+                list.add(line);
+                line = bfr.readLine();
+            }
+            bfr.close();
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public boolean csvChecker(String fileName) {
+        //the number of rows in the file
+        int rowCount = 0;
+
+        //the number of valid rows in the file
+        int validCount = 0;
+
+        //the number of invalid rows in the file
+        int invalidCount = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            while (line != null) {
+                rowCount++;
+                String[] elements = line.split(",");
+
+                //check if the array has exactly 6 elements
+                if (elements.length == 6) {
+                    //check if the first four elements are strings
+                    boolean isString = true;
+                    for (int i = 0; i < 4; i++) {
+                        try {
+                            //try to parse the element as a double
+                            Double.parseDouble(elements[i]);
+                            //if no exception is thrown, then the element is not a string
+                            isString = false;
+                            break;
+                        } catch (NumberFormatException e) {
+                            //if an exception is thrown, then the element is a string
+                            //this helps to guarantee the file rows are formatted correctly
+                            continue;
+                        }
+                    }
+                    //check if the fifth element is an integer
+                    boolean isInt = false;
+                    try {
+                        //try to parse the element as an integer
+                        Integer.parseInt(elements[4]);
+                        //if no exception is thrown, then the element is an integer
+                        isInt = true;
+                        //this helps to guarantee the file rows are formatted correctly
+                    } catch (NumberFormatException e) {
+                        //if an exception is thrown, then the element is not an integer
+                        isInt = false;
+                    }
+                    //check if the sixth element is a double
+                    boolean isDouble = false;
+                    try {
+                        //try to parse the element as a double
+                        Double.parseDouble(elements[5]);
+                        //if no exception is thrown, then the element is a double
+                        //this helps to guarantee the file rows are formatted correctly
+                        isDouble = true;
+                    } catch (NumberFormatException e) {
+                        //if an exception is thrown, then the element is not a double
+                        isDouble = false;
+                    }
+
+                    //if all the checks are passed, then the row is valid
+                    if (isString && isInt && isDouble) {
+                        validCount++;
+                    } else {
+                        invalidCount++;
+                    }
+
+                } else {
+                    //if the array does not have exactly 6 elements, then the row is invalid
+                    invalidCount++;
+                }
+
+                //read the next line of the file and store it in a variable
+                line = br.readLine();
+            }
+
+            //close the buffered reader
+            br.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rowCount == validCount;
+
+    }
+}
+
+
