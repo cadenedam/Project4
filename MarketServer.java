3d8b89d1dc2584c3dab3882a7236d5b144229e32
Rneupane0056 <roshanneupane2003@gmail.com>
2023-04-29 19:12:11 -0400

    Added comments and a header/readMe for the class. The header might need a review/revision.

--- MarketServer.java
+++ MarketServer.java
@@ -5,6 +5,20 @@ import java.util.*;
 import java.lang.Runnable;
 import java.text.*;
 
+/**
+ * MarketServer
+ *
+ * The Thread ArrayList threads is created, and it can be used to start,
+ * stop or stop all threads. Thread thread uses the MarketRun object marketRun
+ * for its parameters. Arraylist threads consists of the thread made with marketRun.
+ *
+ * The populateHashMaps method will grab the username and password
+ * of a user from the users.txt file.
+ *
+ * A new MarketRun object marketRun is created with Socket newSocket, DataInputStream socketReader,
+ * DataOutputStream socketWriter, and MarketServer server.
+ */
+
 public class MarketServer {
 
     private static List<Thread> threads = new ArrayList<>();
@@ -19,9 +33,10 @@ public class MarketServer {
     public static void main(String[] args) throws IOException {
         int port = 4242;
         ServerSocket serverSocket = new ServerSocket(port);
+        //This creates the serverSocket
 
         populateHashMaps();
-
+        //This provides information about the sellers and customers, like their username and password
         MarketServer server = new MarketServer(port);
 
         while (true) {
@@ -33,8 +48,14 @@ public class MarketServer {
                 DataOutputStream socketWriter = new DataOutputStream(newSocket.getOutputStream());
                 
                 MarketRun marketRun = new MarketRun(newSocket, socketReader, socketWriter, server);
+                //This creates a new MarketRun object with Socket newSocket, DataInputStream socketReader,
+                // DataOutputStream socketWriter, and MarketServer server
+
                 Thread thread = new Thread(marketRun);
+                //This creates a new Thread object thread with parameter marketRun
+
                 threads.add(thread);
+                //This adds marketRun to the threads ArrayList
                 thread.start();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -45,16 +66,22 @@ public class MarketServer {
     public static void populateHashMaps() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("users.txt"));
         String line = br.readLine();
+        //Reads through the users.txt file to grab information about the user(s)
 
         while (line != null) {
             String[] splitLine = line.split(";");
 
             if (splitLine[0].equals("Seller")) {
                 sellers.put(splitLine[1], new Sellers(splitLine[1], splitLine[2]));
+                //This grabs the Seller's username and password
             } else {
                 customers.put(splitLine[1], new Customers(splitLine[1], splitLine[2]));
+                //This grabs the Customer's username and password
+                //The only types of users possible are Seller or Customer, so additional
+                //conditional statements are unnecessary
             }
             line = br.readLine();
+            br.close();
         }
     }
 
@@ -73,4 +100,6 @@ public class MarketServer {
             thread.interrupt();
         }
     }
+
+    //End of the class
 }
