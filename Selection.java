d0beda9f699bd379a5d43725d14bc65a0ed4f69b
Rneupane0056 <roshanneupane2003@gmail.com>
2023-04-29 21:12:39 -0400

    Minor changes made to the csvChecker method. I deleted two other methods because their functions were already accomplished within the Sellers and Customers classes.

--- Selection.java
+++ Selection.java
@@ -8,9 +8,6 @@ public class Selection {
     private String filename;
     private List<String> products;
 
-    /// private Customers customers = new Customers(username, password);
-    //these values are always null?
-
 
     File fileProducts = new File("products.txt");
     File filePurchased = new File("purchases.txt");
@@ -40,64 +37,17 @@ public class Selection {
         return list;
     }
 
-    //allows the seller to export products for their store
+    //I deleted the methods that wrote products and purchased files since these tasks
+    //were accomplished within the Sellers, and Customers classes respectively.
+    //updatePurchaseHistory (Customers) writes to the purchased.txt file and updates it in accordance with
+    //the products the customer buys.
+    //The purchased file comes with the Customer username, the store selling the product,
+    //the product name, a description of the product, the quantity available of the product,
+    //and the price of the product.
 
-    public ArrayList<String> writeProductsFile(String filename) throws IOException {
-        ArrayList<String> list = new ArrayList<>();
-        BufferedWriter productListWriter = new BufferedWriter(new FileWriter(fileProducts));
-        String newList = "";
-        productListWriter.write(newList);
-        productListWriter.close();
-        try {
-            FileReader fr = new FileReader(fileProducts);
-            BufferedReader bfr = new BufferedReader(fr);
-            String line = bfr.readLine();
-            while (line != null) {
-                list.add(line);
-                line = bfr.readLine();
-            }
-            bfr.close();
-        } catch (FileNotFoundException e) {
-            throw e;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    //allows the customer to export a file with their purchase history
-
-    public ArrayList<String> writePurchasedFile(String username,String fileName) throws IOException {
-        ArrayList<String> list = new ArrayList<>();
-        BufferedWriter purchaseListWriter = new BufferedWriter(new FileWriter(filename));
-        String newList = "";
-        purchaseListWriter.write(newList);
-        try {
-            FileReader fr = new FileReader(fileName);
-            BufferedReader bfr = new BufferedReader(fr);
-            String line = bfr.readLine();
-            while (line != null) {
-                list.add(line);
-                line = bfr.readLine();
-                if (Objects.equals(list.get(0), "Customer")) {
-                    ArrayList<String> a = readFile("products.txt");
-                    if (a.get(0).equals(username)) {
-                        purchaseListWriter.write(line);
-                    }
-                    //I need to add some additional conditions that display the rows of products
-                    //that customer has bought
-                    //This is a tentative solution
-                }
-            }
-            bfr.close();
-            purchaseListWriter.close();
-        } catch (FileNotFoundException e) {
-            throw e;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
+    //addProduct (Sellers) writes to the products.txt file with new information about the
+    //Seller username, the store selling the product, the product name, a description of the product,
+    //the quantity available of the product, and the price of the product.
 
     public boolean csvChecker(String fileName) {
         //the number of rows in the file
@@ -120,8 +70,9 @@ public class Selection {
                 if (elements.length == 6) {
                     //check if the first four elements are strings
                     boolean isString = true;
-                    for (int i = 0; i < 4; i++) {
+                    for (int i = 0; i < 3; i++) {
                         try {
+
                             //try to parse the element as a double
                             Double.parseDouble(elements[i]);
                             //if no exception is thrown, then the element is not a string
@@ -130,7 +81,6 @@ public class Selection {
                         } catch (NumberFormatException e) {
                             //if an exception is thrown, then the element is a string
                             //this helps to guarantee the file rows are formatted correctly
-                            continue;
                         }
                     }
                     //check if the fifth element is an integer
@@ -143,7 +93,6 @@ public class Selection {
                         //this helps to guarantee the file rows are formatted correctly
                     } catch (NumberFormatException e) {
                         //if an exception is thrown, then the element is not an integer
-                        isInt = false;
                     }
                     //check if the sixth element is a double
                     boolean isDouble = false;
@@ -155,7 +104,6 @@ public class Selection {
                         isDouble = true;
                     } catch (NumberFormatException e) {
                         //if an exception is thrown, then the element is not a double
-                        isDouble = false;
                     }
 
                     //if all the checks are passed, then the row is valid
@@ -180,8 +128,9 @@ public class Selection {
             e.printStackTrace();
         }
         return rowCount == validCount;
-
     }
+
+    //End of the class
 }
 
 
