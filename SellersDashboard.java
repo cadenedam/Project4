c013a6780b92659523cc723acc058a4ba37b17c5
shall1018 <samhall1018@gmail.com>
2023-05-01 12:01:34 -0400

    comments and minor changes to code syntax

--- SellersDashboard.java
+++ SellersDashboard.java
@@ -1,10 +1,8 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import static java.lang.Integer.parseInt;
 
 /**
  * SellersDashboard
@@ -31,11 +29,14 @@ public class SellersDashboard {
 
     public String viewDashboard() throws IOException {
         StringBuilder dashboardString = new StringBuilder();
+        StringBuilder customerData = new StringBuilder();
+        StringBuilder productData = new StringBuilder();
         //String for output of dashboard via GUI
         for (int i = 0; i < stores.length; i++) {
             String storeList;
-            StringBuilder customerData = new StringBuilder();
-            StringBuilder productData = new StringBuilder();
+            customerData.setLength(0);
+            productData.setLength(0);
+            //resets customer and product data for each store
 
             storeList = "Store: " + stores[i];
             //String with the name of the name of the store in the
@@ -63,43 +64,50 @@ public class SellersDashboard {
             }
 
             String[] customers = customerList.toArray(new String[0]);
-            customerData = new StringBuilder("Customer Data\n");
+            customerData.append("Customer Data\n");
             String[] itemsCount = itemsSold.toArray(new String[0]);
             String[] storeProducts = products.toArray(new String[0]);
             boolean printJ = false;
             for (int j = 0; j < customers.length; j++) {
-                customerData.append("Customer:").append(customers[j]).append("Items Purchased: ").append(itemsCount[j]).append("\n");
+                customerData.append("Customer: ").append(customers[j]).append(" - Items Purchased: ").append(itemsCount[j]).append("\n");
                 //This String contains the customer username,
-                // along with the number of items purchased by the customer from purchased.txt.
+                //along with the number of items purchased by the customer from purchased.txt.
                 printJ = true;
             }
             if (!printJ) {
-                customerData = new StringBuilder("~No Customer Data~");
+                customerData.setLength(0);
+                customerData.append("~No Customer Data~");
             }
-            productData = new StringBuilder("\nProduct Data\n");
+            productData.append("\nProduct Data\n");
             boolean printK = false;
             for (int k = 0; k < storeProducts.length; k++) {
+                printK = true;
                 int soldCount = 0;
                 BufferedReader sr = new BufferedReader(new FileReader("purchased.txt"));
                 String countLine;
                 while ((countLine = sr.readLine()) != null) {
                     String[] splitLineSC = countLine.split(";");
                     if (storeProducts[k].equals(splitLineSC[2]) && (stores[i].equals(splitLineSC[1]))) {
+                        //first condition verifies product name and the second ensures that the product is from the
+                        //seller's store and not someone else's who happened to name the product the same name
                         soldCount += Integer.parseInt(splitLineSC[4]);
                     }
                 }
                 sr.close();
-                productData.append("Product:").append(storeProducts[k]).append("Number of Sales: ").append(soldCount).append("\n");
+                productData.append("Product: ").append(storeProducts[k]).append(" - Number of Sales: ").append(soldCount).append("\n");
                 //String with the name of the product, along with the quantity of that product sold.
             }
             if (!printK) {
-                productData = new StringBuilder("~No Product Data~\n");
+                productData.setLength(0);
+                productData.append("~No Product Data~\n");
+                //condition when no data regarding information about sales of any products from a store is present
             }
             dashboardString.append(storeList).append(customerData).append(productData).append("\n");
-
+            //adds customer and product data for each store to the dashboard string to return after the loop of stores
         }
         if (dashboardString.length() == 0) {
-            dashboardString = new StringBuilder("There is no store data.");
+            dashboardString = new StringBuilder();
+            dashboardString.append("There is no store data.");
         }
         return(dashboardString.toString());
         //This returns the string of the SellersDashboard to be viewed in Marketplace
